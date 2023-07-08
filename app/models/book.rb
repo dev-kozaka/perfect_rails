e7765f0a70cc894d77803f722efcd51d3e396d87
dev-kozaka <kozahooo443@yahoo.co.jp>
2023-07-08 10:29:09 +0900

    call back

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,4 +4,26 @@ class Book < ApplicationRecord
   scope :find_price, ->(price) { find_by(price: price) }
 
   belongs_to :publisher
+  has_many :book_authors
+  has_many :authors, through: :book_authors
+
+  validates :name, presence: true
+  validates :name, length: { maximum: 25 }
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+
+  before_validation :add_lovely_to_cat
+
+  def add_lovely_to_cat
+    self.name = self.name.gsub(/Cat/) do |matched|
+       "lovely #{matched}"
+    end
+  end
+
+  after_destroy :if => :high_price? do
+    Rails.logger.info "Book is deleted: #{self.attributes}"
+  end
+
+  def high_price?
+    price >= 5000
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_28_132942) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_03_234236) do
   create_table "authors", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.string "penname"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "book_authors", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "book_id", null: false
+    t.bigint "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_book_authors_on_author_id"
+    t.index ["book_id"], name: "index_book_authors_on_book_id"
   end
 
   create_table "books", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -35,5 +44,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_28_132942) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "book_authors", "authors"
+  add_foreign_key "book_authors", "books"
   add_foreign_key "books", "publishers"
 end
